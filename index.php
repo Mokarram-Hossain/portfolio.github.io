9f5c7b4369359f508b095a3b2999ec73009ed77d
Mokarram Hossain <67956879+Mokarram-Hossain@users.noreply.github.com>
2020-11-02 14:09:50 +0600

    publishing first website
    I am really very glad

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CV portfolio</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" type="text/css" href="css/style.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	  <link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Exo&family=Permanent+Marker&family=Righteous&family=Satisfy&display=swap" rel="stylesheet">
+	  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Shrikhand&family=Tangerine:wght@700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<div class="header" id = "top-header">
+		<nav class="navbar navbar-expand-lg fixed-top p-4">
+			<div class="container text-uppercase p-2">
+			  <a class="navbar-brand text-white font-weight-bold" href="#" style=" letter-spacing: 2px;color: #EA2027!important">Mokarram</a>
+			  <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarColor01">
+			    <ul class="navbar-nav ml-auto">
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Home</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#footer">About</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#services">Services</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#skill">Skill</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#contact-form">Contact</a>
+			      </li>
+			    </ul>
+			 </div>
+			</div>
+		</nav>
+		<section class="header-section">
+			<div class="center-div">
+				<h1 class="font-weight-bold">Mokarram Hossain</h1>
+				<p>Full stack web developer</p>
+				<div class="header-buttons">
+					<a href="#footer">AboutMe</a>
+					<a href="#contact-form" style="background: #EA2027; color: #fff;">Contact</a>
+				</div><br><br>
+			</div>			
+		</section>
+</div>
+
+<!--====================== end of header ============================-->
+
+<!-- ======================end of about =============================-->
+<div class="extra-header" id="services">
+	<div class="container text-left">
+		<h2 class="font-weight-bold text-center py-4 mb-2 text-muted">SERVICES</h2>
+		<div class="row">
+			<div class="extra-div col-lg-4 col-md-4 col-12 text-muted">
+				<a href="#">
+					<i class="fa fa-pencil fa-4x pb-3" area-hidden="true"></i>
+				</a>
+				<h4 class="font-weight-bold pb-1" style="color: #1B1464 !important">WEB DESIGN</h4>
+				<p>We make your design keeping in mind of your choice. You can consult with me and provide me demo if you want. First I show you demo website making in photoshop.</p>
+			</div>
+			<div class="extra-div col-lg-4 col-md-4 col-12 text-muted">
+				<a href="#">
+					<i class="fa fa-code fa-4x font-weight-bold  pb-3" area-hidden="true"></i>
+				</a>
+				<h4 class="font-weight-bold pb-1" style="color: #1B1464 !important">WEB DEVELOPMENT</h4>
+				<p>I develop websites both front end and back end. I develop your website in core PHP and PHP CodeIgniter framework depends on your project size and choice. </p>
+			</div>
+			<div class="extra-div col-lg-4 col-md-4 col-12 text-muted">
+				<a href="#">
+					<i class="fa fa-wordpress fa-4x pb-3" area-hidden="true"></i>
+				</a>
+				<h4 class="font-weight-bold pb-1" style="color: #1B1464 !important">WORDPRESS</h4>
+				<p>I can build your website in wordpress. Basically I use Elementor to customize your website. You can provide theme or give me idea about your website.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="project" id="project"> 
+	<div class="container py-4">
+		<h2 class="font-weight-bold text-center mb-5">MY PROJECTS </h2>
+	</div>
+	<div class="container d-flex justify-content-around align-items-center text-center  mb-5">
+		<div class="pro">
+			<h2 class="count">25</h2>
+			<p>Project done</p>
+		</div>
+		<div class="pro">
+			<h2 class="count">15</h2>
+			<p>Wordpress website</p>
+		</div>
+		<div class="pro">
+			<h2 class="count">5000</h2>
+			<p>Line of coding</p>
+		</div>
+		<div class="pro">
+			<h2 class="count">5</h2>
+			<p>Happy clients</p>
+		</div>
+	</div>
+</section>
+<!-- ===================================== end project=============================================== -->
+<!-- ===========================skill section ====================================-->
+<section class="skill" id="skill">
+	<div class="container">
+		<h2 class="font-weight-bold text-center py-4 mb-2 text-muted">MY SKILL</h2>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-10 offset-1 offset-lg-0">
+				<div class="name my-2">
+					<h6>HTML5</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 95%;background-color: #1B1464 !important;">95%</div>
+						</div>
+				</div><!-- end name -->
+				<div class="name my-2">
+					<h6>CSS3</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 85%;background-color: #1B1464 !important;">85%</div>
+						</div>
+				</div><!-- end name -->
+				<div class="name my-2">
+					<h6>JAVASCRIPT</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 60%;background-color: #1B1464 !important;">60%</div>
+						</div>
+				</div><!-- end name -->
+				<div class="name my-2">
+					<h6>JQUERY</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 60%;background-color: #1B1464 !important;">60%</div>
+						</div>
+				</div><!-- end name -->
+			</div><!-- end col -->
+						<div class="col-lg-6 col-10 offset-1 offset-lg-0">
+				<div class="name my-2">
+					<h6>BOORSTRAP4</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 95%;background-color: #1B1464 !important;">95%</div>
+						</div>
+				</div><!-- end name -->
+				<div class="name my-2">
+					<h6>WORDPRESS</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 85%;background-color: #1B1464 !important;">85%</div>
+						</div>
+				</div><!-- end name -->
+				<div class="name my-2">
+					<h6>PHP (Core)</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 65%;background-color: #1B1464 !important;">65%</div>
+						</div>
+				</div><!-- end name -->
+				<div class="name my-2">
+					<h6>PHP (CodeIgniter)</h6>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 80%;background-color: #1B1464 !important;">80%</div>
+						</div>
+				</div><!-- end name -->
+			</div><!-- end col -->
+		</div><!-- end row -->
+	</div>
+</section><!-- end skill -->
+
+
+<section class="pricing bg-light">
+	<div class="container">
+		<h2 class="font-weight-bold text-center mb-5 text-white text-muted">PRICING</h2>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card text-center">
+					<div class="card-header bg-white font-weight-bold">BASIC</div>
+					<div class="card-body">
+						<li><span class="money">$100</span>/Website</li>
+						<li>Single page website</li>
+						<li>Resposive website</li>
+						<li>Domain free</li>
+						<li>Mobile friendly</li>
+						<li>Webmail support</li>
+						<li>24/7 support</li>
+					</div>
+					<div class="card-footer bg-white p-3">
+						<a href="#" class="btn btn-primary">Purchase</a>
+					</div>
+				</div>
+			</div><!-- end col -->
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card text-center">
+					<div class="card-header bg-white font-weight-bold">STANDARD</div>
+					<div class="card-body">
+						<li><span class="money">$200</span>/Website</li>
+						<li>Multi-page website</li>
+						<li>Resposive website</li>
+						<li>Domain free</li>
+						<li>Mobile friendly</li>
+						<li>Webmail support</li>
+						<li>24/7 support</li>
+					</div>
+					<div class="card-footer bg-white p-3">
+						<a href="#" class="btn btn-primary">Purchase</a>
+					</div>
+				</div>
+			</div><!-- end col -->
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card text-center">
+					<div class="card-header bg-white font-weight-bold">PREMIUM</div>
+					<div class="card-body">
+						<li><span class="money">$300</span>/Website</li>
+						<li>Blog website</li>
+						<li>Resposive website</li>
+						<li>Domain free</li>
+						<li>Mobile friendly</li>
+						<li>Webmail support</li>
+						<li>24/7 support</li>
+					</div>
+					<div class="card-footer bg-white p-3">
+						<a href="#" class="btn btn-primary">Purchase</a>
+					</div>
+				</div>
+			</div><!-- end col -->
+		</div>
+	</div>
+</section>
+<!-- =====================================end pricing ==========================================-->
+<section class="contact-form" id="contact-form">
+
+	<div class="container">
+		<h2 class="font-weight-bold text-center mb-5 text-muted">CONTACT ME</h2>
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-10 offset-lg-2 offset-md-2 col-1">
+				<form action="/action_page.php">
+				  <div class="form-group">
+				    <input type="text" class="form-control" placeholder="Enter Name" id="name" name="name" required autocomplete="off">
+				  </div>
+				  <div class="form-group">
+				    <input type="text" class="form-control" placeholder="Enter Email" id="email" name="email" required autocomplete="off">
+				  </div>
+				  <div class="form-group">
+				    <input type="text" class="form-control" placeholder="Enter Number" id="number" name="number" required autocomplete="off">
+				  </div>
+				  <div class="form-group">
+				    <textarea class ="form-control" name="message" placeholder="Enter Message" rows="4"></textarea>
+				  </div>
+				  <div class="d-flex justify-content-center">
+				  	<br>
+				  <button type="submit" class="btn btn-primary">Submit</button>
+				  </div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- ================================end contact-form ==========================-->
+
+<section class="footer-section" id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12 text-center">
+				<h3 class="font-weight-bold pb-2">ABOUT ME</h3>
+				<img src="images/abt-pic.jpg" class="img-fluid" style="border-radius: 50% !important; width: 130px;height: 130px">
+				<p>Hi, this is Mokarram Hossain. I'm full stack web developer. For front end developing I use HTML, CSS, BOOTSTRAP, JQUERY. For backend I use core PHP and CodeIgniter PHP framework.</p><br>
+
+			</div>
+			<div class="col-lg-4 col-md-4 col-12 text-center">
+				<h3 class="font-weight-bold pb-2">LINKS</h3>
+				<li><a href="#top-header">Home</a></li> <br>
+				<li><a href="#about">About</a></li> <br>
+				<li><a href="#services">Services</a></li> <br>
+				<li><a href="#skill">Skill</a></li> <br>
+				<li><a href="#contact-form">Contact</a></li>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12 text-center">
+				<h3 class="font-weight-bold pb-2">SUPPORT</h3>
+				Phone : +8801723155254 <br> <br>
+				Phone : +88013037341168 <br> <br>
+				Email : mokarram254@gmail.com <br> <br>
+				Website: www.cravyfood.epizy.com 	<br><br> <br>
+				<a href="https://www.facebook.com/mokarram.hossain.733/"><i class="fa-2x fa fa-facebook"></i></a>&nbsp;&nbsp;
+				<i class="fa-2x fa fa-whatsapp"></i>&nbsp;&nbsp;
+				<i class="fa-2x fa fa-instagram"></i>&nbsp;&nbsp;
+				<i class="fa-2x fa fa-twitter"></i>
+			</div>
+			
+		</div>
+		<br><br>
+		<p class="text-center">Copyright©2020 All rights reserved | This website is made by Mokarram Hossain | For more information : 01303734168 | mokarram254@gmail.com</p>
+	</div>
+</section>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>	
+
+<script>
+	$('.count').counterUp({
+		delay : 10,
+		time : 3000
+	})
+
+
+</script>
+
+</body>
+</html>
